8b9565c573b9902d6f438acdb73aa9c4ad2ee42b
Caio <108128119+DocCaio@users.noreply.github.com>
2024-07-11 17:31:04 -0300

    up

--- src/app/pages/ticket/page.tsx
+++ src/app/pages/ticket/page.tsx
@@ -1,34 +1,63 @@
-import styles from './ticket.module.css'; // Make sure the file path is correct
-import Link from 'next/link';
+"use client";
+import { useState } from 'react';
+import styles from './ticket.module.css'; 
 import Image from 'next/image';
-import Poltrona from '../../assets/PoltronaLivre.png';
-import Preferencial from '../../assets/PoltronaLivrePreferencial.png';
+import PoltronaLivre from '../../assets/PoltronaLivre.png';
+import PoltronaOcupada from '../../assets/PoltronaEscolhida.png';
+import PreferencialLivre from '../../assets/PoltronaLivrePreferencial.png';
+import PreferencialOcupada from '../../assets/PoltronaEscolhidaPreferencial.png';
 import More from './More';
-import Vips from './Vips'
+import Vips from './Vips';
 
 export default function Ticket() {
+  const [selectedNormalImages, setSelectedNormalImages] = useState<boolean[]>(Array(10).fill(false));
+  const [selectedPreferentialImages, setSelectedPreferentialImages] = useState<boolean[]>(Array(2).fill(false));
+
+  const handleNormalClick = (index: number) => {
+    setSelectedNormalImages((prev) => {
+      const newSelectedImages = [...prev];
+      newSelectedImages[index] = !newSelectedImages[index];
+      return newSelectedImages;
+    });
+  };
+
+  const handlePreferentialClick = (index: number) => {
+    setSelectedPreferentialImages((prev) => {
+      const newSelectedImages = [...prev];
+      newSelectedImages[index] = !newSelectedImages[index];
+      return newSelectedImages;
+    });
+  };
+
   return (
-       <>           
-    <section>
-      <Vips/>
-      <div  className={styles.chars}>
-      <div className={styles.ticketContainer}>
-        {[...Array(10)].map((_, index) => (
-          <div key={index} className={styles.imageWrapper}>            
-            <Image src={Poltrona} alt={`Poltrona ${index + 1}`} width={100} height={100} />
-
+    <>           
+      <section>
+        <Vips />
+        <div className={styles.chars}>
+          <div className={styles.ticketContainer}>
+            {[...Array(10)].map((_, index) => (
+              <div key={index} className={styles.imageWrapper} onClick={() => handleNormalClick(index)}>                      
+                <Image
+                  src={selectedNormalImages[index] ? PoltronaOcupada : PoltronaLivre}
+                  alt={`Poltrona ${index + 1}`}
+                  width={100}
+                  height={100}
+                />
+              </div>
+            ))}
+            {[...Array(2)].map((_, index) => (
+              <div key={index} className={styles.imageWrapper} onClick={() => handlePreferentialClick(index)}>              
+                <Image
+                  src={selectedPreferentialImages[index] ? PreferencialOcupada : PreferencialLivre}
+                  alt={`PoltronaPreferencial ${index + 1}`}
+                  width={100}
+                  height={100}
+                />
+              </div>
+            ))}
           </div>
-        ))}
-        {[...Array(2)].map((_, index) => (
-          <div key={index} className={styles.imageWrapper}>            
-            <Image src={Preferencial} alt={`Preferencial ${index + 1}`} width={100} height={100} />
-
-          </div>
-        ))}
-      </div>
-
-      <More/>
-      </div>
+          <More />
+        </div>
       </section>
     </>
   );
