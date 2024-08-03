1b6ebb8ec79c111fce394ecfa714d96732d60c67
Caio <108128119+DocCaio@users.noreply.github.com>
2024-08-03 10:12:42 -0300

    up

--- src/components/carrinho/AreaItemCarrinho.tsx
+++ src/components/carrinho/AreaItemCarrinho.tsx
@@ -1,6 +1,7 @@
 import ItemCarrinho from '@/data/model/ItemCarrinho';
 import { IconMinus, IconPlus, IconX } from '@tabler/icons-react';
 import Image from 'next/image';
+import styles from './styles/areaItem.module.css';
 
 export interface AreaItemCarrinhoProps {
     item: ItemCarrinho
@@ -10,33 +11,33 @@ export interface AreaItemCarrinhoProps {
 
 export default function AreaItemCarrinho(props: AreaItemCarrinhoProps) {
     return (
-        <div className="flex items-center gap-5 bg-zinc-900 rounded-md overflow-hidden">
-            <div className="relative w-28 h-28">
+        <div className={styles.container}>
+            <div className={styles.card}>
                 <Image
                     src={props.item.produto.imagem}
                     alt={props.item.produto.nome}
                     fill
-                    className="object-cover"
+                    className={styles.img}
                 />
             </div>
-            <div className="flex flex-col justify-center flex-1">
-                <span className="text-xl font-bold">{props.item.produto.nome}</span>
-                <span className="text-sm text-zinc-400">{props.item.produto.descricao}</span>
-                <div className="flex items-center gap-2 mt-2 text-zinc-400 text-lg font-bold">
+            <div className={styles.description}>
+                <span className={styles.descriptionName}>{props.item.produto.nome}</span>
+                <span className={styles.descriptionProduct}>{props.item.produto.descricao}</span>
+                <div className={styles.contain}>
                     <span>R$ {props.item.produto.preco.toFixed(2)}</span>
                     <IconX size={20} />
                     <span>{props.item.quantidade}</span>
                     <span>=</span>
-                    <span className="text-yellow-500">
+                    <span className={styles.containQnt}>
                         R$ {(props.item.produto.preco * props.item.quantidade).toFixed(2)}
                     </span>
                 </div>
             </div>
-            <div className="flex gap-2 items-center px-5">
+            <div className={styles.remove}>
                 <button onClick={() => props.remover?.(props.item)}>
                     <IconMinus />
                 </button>
-                <span className="flex px-4 py-2 rounded-md bg-black">{props.item.quantidade}</span>
+                <span className={styles.Qnt}>{props.item.quantidade}</span>
                 <button onClick={() => props.adicionar?.(props.item)}>
                     <IconPlus />
                 </button>
