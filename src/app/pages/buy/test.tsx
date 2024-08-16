67f75612f2931e61a57cb277cb06b58eafa20056
Caio <108128119+DocCaio@users.noreply.github.com>
2024-08-16 17:24:15 -0300

    up

---
+++ src/app/pages/buy/test.tsx
@@ -0,0 +1,85 @@
+/*"use client";
+import { useState } from 'react';
+import styles from './ticket.module.css';
+import Image from 'next/image';
+import PoltronaLivre from '../../assets/PoltronaLivre.png';
+import PoltronaOcupada from '../../assets/PoltronaEscolhida.png';
+import PreferencialLivre from '../../assets/PoltronaLivrePreferencial.png';
+import PreferencialOcupada from '../../assets/PoltronaEscolhidaPreferencial.png';
+import More from './More';
+import Vips from './Vips';
+import Legend from './Legend';
+import Screen from './Screen';
+import Link from 'next/link';
+
+export default function Ticket() {
+  const [selectedNormalImages, setSelectedNormalImages] = useState<boolean[]>(Array(10).fill(false));
+  const [selectedPreferentialImages, setSelectedPreferentialImages] = useState<boolean[]>(Array(2).fill(false));
+
+  const handleNormalClick = (index: number) => {
+    setSelectedNormalImages((prev) => {
+      const newSelectedImages = [...prev];
+      newSelectedImages[index] = !newSelectedImages[index];
+      return newSelectedImages;
+    });
+  };
+
+  const handlePreferentialClick = (index: number) => {
+    setSelectedPreferentialImages((prev) => {
+      const newSelectedImages = [...prev];
+      newSelectedImages[index] = !newSelectedImages[index];
+      return newSelectedImages;
+    });
+  };
+
+  // Verifica se pelo menos uma imagem está selecionada
+  const isButtonEnabled = selectedNormalImages.some(Boolean) || selectedPreferentialImages.some(Boolean);
+
+  return (
+    <>
+      <section>
+        <div>
+          <Legend/>
+        </div>
+        <Vips/>
+        <div className={styles.chars}>
+          <div className={styles.ticketContainer}>
+            {[...Array(10)].map((_, index) => (
+              <div key={index} className={styles.imageWrapper} onClick={() => handleNormalClick(index)}>
+                <Image
+                  className={styles.sits}
+                  src={selectedNormalImages[index] ? PoltronaOcupada : PoltronaLivre}
+                  alt={`Poltrona ${index + 1}`}
+                  width={100}
+                  height={100}
+                />
+              </div>
+            ))}
+            {[...Array(2)].map((_, index) => (
+              <div key={index} className={styles.imageWrapper} onClick={() => handlePreferentialClick(index)}>
+                <Image
+                  className={styles.chairWheels}
+                  src={selectedPreferentialImages[index] ? PreferencialOcupada : PreferencialLivre}
+                  alt={`PoltronaPreferencial ${index + 1}`}
+                  width={100}
+                  height={100}
+                />
+              </div>
+            ))}
+          </div>
+          <More/>
+        </div>
+        <Screen/>
+
+        <div className={styles.link}>
+          <Link
+            href="../../pages/shop"
+            className={`${styles.next} ${isButtonEnabled ? '' : styles.disabled}`}
+          >
+            Next
+          </Link>
+        </div>
+      </section>
+    </>
+  );
+}*/
--- src/app/pages/chairs/page.tsx
+++ src/app/pages/chairs/page.tsx
@@ -1,4 +1,5 @@
-"use client";
+
+"use client"
 import { useState } from 'react';
 import styles from './ticket.module.css';
 import Image from 'next/image';
@@ -6,15 +7,16 @@ import PoltronaLivre from '../../assets/PoltronaLivre.png';
 import PoltronaOcupada from '../../assets/PoltronaEscolhida.png';
 import PreferencialLivre from '../../assets/PoltronaLivrePreferencial.png';
 import PreferencialOcupada from '../../assets/PoltronaEscolhidaPreferencial.png';
-import More from './More';
-import Vips from './Vips';
 import Legend from './Legend';
 import Screen from './Screen';
 import Link from 'next/link';
 
 export default function Ticket() {
-  const [selectedNormalImages, setSelectedNormalImages] = useState<boolean[]>(Array(10).fill(false));
-  const [selectedPreferentialImages, setSelectedPreferentialImages] = useState<boolean[]>(Array(2).fill(false));
+  const totalNormalSeats = 30; // Quantidade total de assentos normais
+  const totalPreferentialSeats = 6; // Quantidade total de assentos preferenciais
+  
+  const [selectedNormalImages, setSelectedNormalImages] = useState<boolean[]>(Array(totalNormalSeats).fill(false));
+  const [selectedPreferentialImages, setSelectedPreferentialImages] = useState<boolean[]>(Array(totalPreferentialSeats).fill(false));
 
   const handleNormalClick = (index: number) => {
     setSelectedNormalImages((prev) => {
@@ -41,13 +43,11 @@ export default function Ticket() {
         <div>
           <Legend/>
         </div>
-        <Vips/>
         <div className={styles.chars}>
-          <div className={styles.ticketContainer}>
-            {[...Array(10)].map((_, index) => (
-              <div key={index} className={styles.imageWrapper} onClick={() => handleNormalClick(index)}>
+          <div className={styles.vip}>
+            {[...Array(12)].map((_, index) => (
+              <div key={index} className={styles.imageWrapper} onClick={() => handleNormalClick(index)}>                      
                 <Image
-                  className={styles.sits}
                   src={selectedNormalImages[index] ? PoltronaOcupada : PoltronaLivre}
                   alt={`Poltrona ${index + 1}`}
                   width={100}
@@ -56,9 +56,8 @@ export default function Ticket() {
               </div>
             ))}
             {[...Array(2)].map((_, index) => (
-              <div key={index} className={styles.imageWrapper} onClick={() => handlePreferentialClick(index)}>
+              <div key={index + totalNormalSeats} className={styles.imageWrapper} onClick={() => handlePreferentialClick(index)}>              
                 <Image
-                  className={styles.chairWheels}
                   src={selectedPreferentialImages[index] ? PreferencialOcupada : PreferencialLivre}
                   alt={`PoltronaPreferencial ${index + 1}`}
                   width={100}
@@ -66,8 +65,57 @@ export default function Ticket() {
                 />
               </div>
             ))}
+          </div>        
+        </div>
+        <div className={styles.chars}>
+          <div className={styles.ticketContainer}>
+            {[...Array(10)].map((_, index) => (
+              <div key={index + 12} className={styles.imageWrapper} onClick={() => handleNormalClick(index + 12)}>
+                <Image
+                  className={styles.sits}
+                  src={selectedNormalImages[index + 12] ? PoltronaOcupada : PoltronaLivre}
+                  alt={`Poltrona ${index + 13}`}
+                  width={100}
+                  height={100}
+                />
+              </div>
+            ))}
+            {[...Array(2)].map((_, index) => (
+              <div key={index + totalNormalSeats + 2} className={styles.imageWrapper} onClick={() => handlePreferentialClick(index + 2)}>
+                <Image
+                  className={styles.chairWheels}
+                  src={selectedPreferentialImages[index + 2] ? PreferencialOcupada : PreferencialLivre}
+                  alt={`PoltronaPreferencial ${index + 3}`}
+                  width={100}
+                  height={100}
+                />
+              </div>
+            ))}
           </div>
-          <More/>
+          <div className={styles.ticketContainer}>
+            {[...Array(10)].map((_, index) => (
+              <div key={index + 22} className={styles.imageWrapper} onClick={() => handleNormalClick(index + 22)}>
+                <Image
+                  className={styles.sits}
+                  src={selectedNormalImages[index + 22] ? PoltronaOcupada : PoltronaLivre}
+                  alt={`Poltrona ${index + 23}`}
+                  width={100}
+                  height={100}
+                />
+              </div>
+            ))}
+            {[...Array(2)].map((_, index) => (
+              <div key={index + totalNormalSeats + 4} className={styles.imageWrapper} onClick={() => handlePreferentialClick(index + 4)}>
+                <Image
+                  className={styles.chairWheels}
+                  src={selectedPreferentialImages[index + 4] ? PreferencialOcupada : PreferencialLivre}
+                  alt={`PoltronaPreferencial ${index + 5}`}
+                  width={100}
+                  height={100}
+                />
+              </div>
+            ))}
+          </div>
         </div>
         <Screen/>
 
