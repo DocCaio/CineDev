df640ca4715df7fd1da6f09ab43ea9ea1366b70f
Caio <108128119+DocCaio@users.noreply.github.com>
2024-08-21 16:51:10 -0300

    up

--- src/app/pages/Upcoming/page.tsx
+++ src/app/pages/Upcoming/page.tsx
@@ -2,7 +2,7 @@
 "use client";
 import React, { useEffect, useState } from 'react';
 import Card from './Posters/poster';
-import { Card as CardType } from '../../../data/model/Poster';
+import { Card as CardType } from '@/data/model/Poster';
 import cardData from '@/data/utils/Upcoming.json'; // Supondo que cards.json esteja na mesma pasta
 import styles from './Upcoming.module.css';
 
