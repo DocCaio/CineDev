ac6de85d752ac73b93194dfaa45ee5167b3ceae3
Caio <108128119+DocCaio@users.noreply.github.com>
2024-08-22 09:28:10 -0300

    up

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,79 +1,65 @@
 import Link from "next/link";
 import styles from '../produto/CardProduto.module.css';
 
-
-
 interface ModalProps {
   isOpen: boolean;
+  setModalOpen: () => void;
+  children: React.ReactNode;
 }
-  
 
 const Bg_Style = {
-    position: 'fixed',
-    top: '0',
-    right: '0',
-    left: '0',
-    bottom: '0',
-    backgroundColor: 'rgba(0,0,0,0.7)',
-    zIndex:'1000'
-}
+  position: 'fixed',
+  top: '0',
+  right: '0',
+  left: '0',
+  bottom: '0',
+  backgroundColor: 'rgba(0,0,0,0.7)',
+  zIndex: '1000'
+};
 
+const modal_style = {
+  display: 'block',
+  gap: '10px',
+  alignItems: 'center',
+  position: 'fixed',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  padding: '100px',
+  backgroundColor: '#fff',
+  borderRadius: '10px',
+  color: 'black'
+};
 
-const modal_style = { 
-    display:'block',
-    gap:'10px',  
-    alignItems: 'center',
-    position: 'fixed',
-    top: '50%',
-    left: '50%',
-    transform:'translate(-50%, -50%)' ,
-    padding:'100px' ,
-    backgroundColor:'#fff',
-    borderRadius:'10px',
-    color:'black'
-    
-  
-   }
-  
-   
-   const close_bnt = { 
-    cursor: 'pointer',
-    color: 'white',
-    backgroundColor: '#1D4ED8',  
-    fontWeight: '500',
-    borderRadius: '0.5rem',
-    fontSize: '1rem',
-    lineHeight: '2rem',
-    padding: ' 0.7rem 2rem',  
-    textAlign: 'center',
-    marginRight: '0.5rem',
-    display: 'inline-flex',
-    alignItems: 'center',
-    TextDecoration:'none',
-    border: 'none'  
-  
-   }
+const close_bnt = {
+  cursor: 'pointer',
+  color: 'white',
+  backgroundColor: '#1D4ED8',
+  fontWeight: '500',
+  borderRadius: '0.5rem',
+  fontSize: '1rem',
+  lineHeight: '2rem',
+  padding: '0.7rem 2rem',
+  textAlign: 'center',
+  marginRight: '0.5rem',
+  display: 'inline-flex',
+  alignItems: 'center',
+  textDecoration: 'none',
+  border: 'none'
+};
 
-      
- 
-   
-
-const Modal: React.FC<ModalProps> = ({ isOpen, children }) => {
+const Modal: React.FC<ModalProps> = ({ isOpen, setModalOpen, children }) => {
   if (isOpen) {
-    return  (
-        <div style={Bg_Style}>
-       <div style={modal_style}>
-        
-       <div>{children}</div>
-       <Link className={styles.link} href='/'
-            style={close_bnt}>Back
-      </Link>
-                    
+    return (
+      <div style={Bg_Style}>
+        <div style={modal_style}>
+          <div>{children}</div>
+          <Link className={styles.link} href='/' style={close_bnt} onClick={setModalOpen}>
+            Back
+          </Link>
         </div>
-       </div >       
-       
-
-    )
+      </div>
+    );
   }
   return null;
 };
