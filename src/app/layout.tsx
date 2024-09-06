f31a9ba7423e2040bdd96b049fb2ae932428f34e
Caio <108128119+DocCaio@users.noreply.github.com>
2024-09-06 15:09:55 -0300

    error fixed
    slow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={style.body}>
         <header>          
           <nav className={style.barContent} >   
            <Navbar/>   
